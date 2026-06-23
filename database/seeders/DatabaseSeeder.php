db1f1dd338ff386dae8e3edd28191172cb20f068
Santiago Michel <santiagomichel2604@gmail.com>
2026-06-23 19:56:14 -0300

    agrego configuracion de volumnes file server para cada entorno correspondiente

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             CategoriasSeeder::class,
-            ProductosSeeder::class,
+            /*             ProductosSeeder::class,
+ */
         ]);
     }
 }
